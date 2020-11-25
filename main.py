c6b73036cbb18f1750dc3a91735e0c9df2890f78
cit007 <itcho0077@gmail.com>
2020-11-25 23:38:31 +0900

    make data using pandas lib: feed data is csv

---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+daily_all = pd.read_csv("data/daily_report.csv")
+conditions = ["confirmed", "deaths", "recovered"]
+
+
+def make_time_df(condition):
+    time_df = pd.read_csv(f"data/time_{condition}.csv")
+    time_sum_df = time_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
+    ).reset_index(name=f"{condition}").rename(columns={"index": "date"})
+    return time_sum_df
+
+
+def make_all_conditions_time_df():
+    # daily info(confirmed/death/recover)
+    acc_df = None
+    for condition in conditions:
+        df = make_time_df(condition)
+        if acc_df is None:
+            acc_df = df
+        else:
+            acc_df = acc_df.merge(df)
+    return acc_df
+
+
+def make_country_df(condition, country):
+    country_df = pd.read_csv(f"data/time_{condition}.csv")
+    # get row info : loc[df[true]]
+    country_df = country_df.loc[country_df["Country/Region"] == country]
+    country_sum = country_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
+    ).reset_index(name=f"{condition}").rename(columns={"index": "date"})
+    return country_sum
+
+
+def make_all_conditions_country_df(country):
+    acc_df = None
+    for condition in conditions:
+        df = make_country_df(condition, country)
+        if acc_df is None:
+            acc_df = df
+        else:
+            acc_df = acc_df.merge(df)
+    acc_df
+
+
+# by all
+daily_total_df = daily_all[["Confirmed", "Deaths", "Recovered"]]
+total_sum = daily_total_df.sum().reset_index().rename(
+    columns={"index": "condition"})
+
+# by country
+daily_country_df = daily_all[["Country_Region",
+                              "Confirmed", "Deaths", "Recovered"]]
+country_sum = daily_country_df.groupby("Country_Region").sum().reset_index()
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. The site link where Nico visits for him to check the status of COVID 19 :
+   https://bit.ly/31YewhF
+
+2. daily-reports
+   https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
+
+3. time-series
+   https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
