3771b8ea308656418896a612b327379b88a2db51
cit007 <itcho0077@gmail.com>
2020-11-26 00:51:07 +0900

    feed data modify

--- data.py
+++ data.py
@@ -6,41 +6,9 @@ conditions = ["confirmed", "deaths", "recovered"]
 
 def make_time_df(condition):
     time_df = pd.read_csv(f"data/time_{condition}.csv")
-    time_sum_df = time_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
+    time_sum = time_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
     ).reset_index(name=f"{condition}").rename(columns={"index": "date"})
-    return time_sum_df
-
-
-def make_all_conditions_time_df():
-    # daily info(confirmed/death/recover)
-    acc_df = None
-    for condition in conditions:
-        df = make_time_df(condition)
-        if acc_df is None:
-            acc_df = df
-        else:
-            acc_df = acc_df.merge(df)
-    return acc_df
-
-
-def make_country_df(condition, country):
-    country_df = pd.read_csv(f"data/time_{condition}.csv")
-    # get row info : loc[df[true]]
-    country_df = country_df.loc[country_df["Country/Region"] == country]
-    country_sum = country_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
-    ).reset_index(name=f"{condition}").rename(columns={"index": "date"})
-    return country_sum
-
-
-def make_all_conditions_country_df(country):
-    acc_df = None
-    for condition in conditions:
-        df = make_country_df(condition, country)
-        if acc_df is None:
-            acc_df = df
-        else:
-            acc_df = acc_df.merge(df)
-    acc_df
+    return time_sum
 
 
 # by all
@@ -52,3 +20,34 @@ total_sum = daily_total_df.sum().reset_index().rename(
 daily_country_df = daily_all[["Country_Region",
                               "Confirmed", "Deaths", "Recovered"]]
 country_sum = daily_country_df.groupby("Country_Region").sum().reset_index()
+
+# daily info(confirmed/death/recover)
+acc_df = None
+for condition in conditions:
+    df = make_time_df(condition)
+    if acc_df is None:
+        acc_df = df
+    else:
+        acc_df = acc_df.merge(df)
+
+
+# -------
+
+
+def make_country_df(condition, country):
+    country_df = pd.read_csv(f"data/time_{condition}.csv")
+    country_df = country_df.loc[["Country/Region"] == country]
+    country_sum = country_df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum(
+    ).reset_index(name=f"{condition}").rename(columns={"index": "date"})
+    return country_sum
+
+
+acc_df = None
+for condition in conditions:
+    df = make_country_df(condition)
+    if acc_df is None:
+        acc_df = df
+    else:
+        acc_df = acc_df.merge(df)
+
+acc_df
