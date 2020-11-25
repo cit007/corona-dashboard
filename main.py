524e9b199e64b0196e5bc5dec2084fd37d1fbd6d
cit007 <itcho0077@gmail.com>
2020-11-26 01:16:20 +0900

    basic structure for html/css

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 
-app = dash.Dash(__name__)
-app.layout = html.Div(html.H1("corona dashboard"))
+stylesheets = ["https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css"]
+app = dash.Dash(__name__, external_stylesheets=stylesheets)
+app.layout = html.Div(
+    style={"minHeight": "100vh",
+           "backgroundColor": "black", "color": "white", "fontSize": "2rem"},
+    children=[html.Header(
+        style={"textAlign": "center", "paddingTop": "50px"},
+        children=[html.H1("corona dashboard")]
+    )],
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
